ee5e54526500210f6a7e104584cc7ee715703ec5
genoi44444 <77691261+genoi44444@users.noreply.github.com>
2026-01-06 17:59:38 +0200

    save

--- SmartHouse/SmartHouse/DeviceHandlers/SchedualDeviceHandler.cs
+++ SmartHouse/SmartHouse/DeviceHandlers/SchedualDeviceHandler.cs
@@ -6,12 +6,26 @@ using System.Threading.Tasks;
 
 namespace SmartHouse
 {
-    public class SchedualDeviceHandler : DeviceHandler
+    public class SchedualDeviceHandler<T> : DeviceHandler<T> where T : ISchedualDevice
     {
-        public SchedualDeviceHandler(IJsonDataBase<IDevice> dataDevice) : base(dataDevice)
+        
+        public SchedualDeviceHandler(IJsonDataBase<T> dataDevice) : base(dataDevice)
         {
         }
+        public async Task<Dictionary<DateTime, bool>> GetSchedual(string id)
+        {
+            ISchedualDevice device = await _deviceDB.GetItemInfo(id);
 
+            if (device == null)
+                throw new Exception($"Device with id {id} not found");
+
+            return device.SchedualTime;
+        }
+        public async Task RemoveFromSchedual(string id,DateTime datetime )
+        {
+
+        }
+        
 
     }
 }
