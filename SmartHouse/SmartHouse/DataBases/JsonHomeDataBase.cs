84adec0b8d71b068c779f1bca5858d9964f27d88
genoi44444 <77691261+genoi44444@users.noreply.github.com>
2026-01-06 18:52:37 +0200

    save

--- SmartHouse/SmartHouse/DataBases/JsonHomeDataBase.cs
+++ SmartHouse/SmartHouse/DataBases/JsonHomeDataBase.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace SmartHouse
 {
@@ -10,19 +13,67 @@ namespace SmartHouse
     {
         private readonly string  _jsonPath="homejsonDB.json";
         List<IDevice> _devices;
+        private readonly JsonSerializerSettings _settings = new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All,
+            Formatting = Formatting.Indented
+        };
+        public JsonHomeDataBase()
+        {
+            LoadFromFile();
+        }
+
         public async Task<IDevice> GetItemInfo(string id)
         {
-            throw new NotImplementedException();
+            // Search the local list for the device with matching ID
+            return await Task.Run(() => _devices.FirstOrDefault(d => d._id == id));
         }
 
         public async Task SaveToDB(IDevice item)
         {
-            throw new NotImplementedException();
+            if (_devices.Any(d => d._id == item._id))
+                throw new Exception("Device with this ID already exists.");
+
+            _devices.Add(item);
+            await SaveToFile();
+        }
+        private async Task SaveToFile()
+        {
+            string json = JsonConvert.SerializeObject(_devices, _settings);
+            // Use the standard synchronous method
+            File.WriteAllText(_jsonPath, json);
+            await Task.CompletedTask;
+
         }
 
         public async Task UpdateDB(IDevice item)
         {
-            throw new NotImplementedException();
+            var index = _devices.FindIndex(d => d._id == item._id);
+
+            if (index == -1)
+                throw new Exception("Device not found.");
+
+            _devices[index] = item;
+            await SaveToFile();
         }
+        private void LoadFromFile()
+        {
+            if (!File.Exists(_jsonPath))
+            {
+                _devices = new List<IDevice>();
+                return;
+            }
+
+            try
+            {
+                string json = File.ReadAllText(_jsonPath);
+                _devices = JsonConvert.DeserializeObject<List<IDevice>>(json, _settings) ?? new List<IDevice>();
+            }
+            catch
+            {
+                _devices = new List<IDevice>();
+            }
+        }
+
     }
 }
